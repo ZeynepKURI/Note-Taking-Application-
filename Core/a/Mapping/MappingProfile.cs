06287118248f77e872cbeb84474e0ef4427bb99d
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2025-01-08 19:32:27 +0300

    son

---
+++ Core/a/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Application.DTOs;
+using AutoMapper;
+using Domain.Enitities;
+
+namespace Application.Mapping
+{
+	public class MappingProfile : Profile
+	{
+        public MappingProfile()
+        {
+            // Entity -> DTO dönüşümü
+            CreateMap<Note, NoteDTO>().ReverseMap();
+        }
+    }
+}
+
--- Presentation/Api/Controllers/UserController.cs
+++ Presentation/Api/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,85 +10,12 @@ namespace Api.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult Index()
+        private readonly IAuthService authService;
+
+        public UserController(IAuthService _authService)
         {
-            return View();
+            authService = _authService;
         }
 
-        // GET: User/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: User/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: User/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: User/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: User/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: User/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: User/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
