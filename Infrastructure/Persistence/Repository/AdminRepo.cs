e30b95cb64324c0b0a44886e31d9a5c4d27457b1
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2025-01-10 11:54:17 +0300

    güncell

--- Infrastructure/Persistence/Repository/AdminRepo.cs
+++ Infrastructure/Persistence/Repository/AdminRepo.cs
@@ -25,19 +25,38 @@ namespace Persistence.Repository
             this.appDbContext = appDbContext;
         }
 
-        private async Task<Admin> FindAdminByEmail(string email) =>
-        await appDbContext.admins.FirstOrDefaultAsync(u => u.Email == email);
+       
 
-        private string GenerateJWTToken(User user)
+
+        public async Task<LoginResponse> LoginAsync(LoginDTO loginDTO)
+        {
+            // Kullanıcıyı e-posta ile bulalım
+            var getAdmin = await FindAdminByEmail(loginDTO.Email!);
+            if (getAdmin == null)
+                return new LoginResponse(false, "Admin not found");
+
+            // Şifreyi doğrulayalım
+            bool checkPassword = BCrypt.Net.BCrypt.Verify(loginDTO.Password, getAdmin.Password);
+            if (checkPassword)
+                return new LoginResponse(true, "Login successful", GenerateJWTToken(getAdmin));
+            else
+                return new LoginResponse(false, "Invalid credentials");
+        }
+
+
+        private async Task<Admin> FindAdminByEmail(string email) =>
+       await appDbContext.admins.FirstOrDefaultAsync(u => u.Email == email);
+
+        private string GenerateJWTToken(Admin admins)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             var userClaims = new[]
             {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Username!),
-                new Claim(ClaimTypes.Email, user.Email!)
+                new Claim(ClaimTypes.NameIdentifier, admins.Id.ToString()),
+                new Claim(ClaimTypes.Name, admins.Username!),
+                new Claim(ClaimTypes.Email, admins.Email!)
             };
 
             var token = new JwtSecurityToken(
@@ -52,30 +71,11 @@ namespace Persistence.Repository
         }
 
 
-        public async Task<LoginResponse> LoginAsync(LoginDTO loginDTO)
-        {
-            // Kullanıcıyı e-posta ile bulalım
-            var getAdmin = await FindAdminByEmail(loginDTO.Email!);
-            if (getAdmin == null)
-                return new LoginResponse(false, "Admin not found");
-
-            // Şifreyi doğrulayalım
-            bool checkPassword = BCrypt.Net.BCrypt.Verify(loginDTO.Password, getAdmin.Password);
-            if (checkPassword)
-                return new LoginResponse(true, "Login successful");
-            else
-                return new LoginResponse(false, "Invalid credentials");
-        }
-
-
-
-
-      
 
         public async Task<RegisterResponse> RegisterAsync(RegisterDTO registerDTO)
         {
-            var getUser = await FindAdminByEmail(registerDTO.Email!);
-            if (getUser != null)
+            var getAdmin= await FindAdminByEmail(registerDTO.Email!);
+            if (getAdmin!= null)
                 return new RegisterResponse(false, "User already exists");
 
             appDbContext.admins.Add(new Admin()
